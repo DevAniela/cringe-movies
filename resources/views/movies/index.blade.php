53f10ddaed8d3327b0399179ba5b0c4bbe7e58f6
DevAniela <dismal.damsel@gmail.com>
2025-12-08 10:19:17 +0200

    feat: Implement READ logic (index) for Movies and basic MovieController

---
+++ resources/views/movies/index.blade.php
@@ -0,0 +1,35 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2>{{ __('Cringe Movies') }}</h2>
+    </x-slot>
+
+    <div>
+        <h3>Movie List:</h3>
+
+        @if($movies->isEmpty())
+            <p>No movies have been added yet. Be the first to add one!</p>
+        @else
+            <ul>
+                @foreach($movies as $movie)
+                    <li>
+                        <h4>{{ $movie->title }} ({{ $movie->release_year }})</h4>
+
+                        <p>Added by: **{{ $movie->user->name ?? 'Unknown' }}**
+                            @if($movie->category)
+                                | Category: {{ $movie->category->name }}
+                            @endif
+                        </p>
+
+                        <p>{{ Str::limit($movie->description, 100) }}</p>
+                    </li>
+                @endforeach
+            </ul>
+
+            <div>
+                {{ $movies->links() }}
+            </div>
+            
+        @endif
+
+    </div>
+</x-app-layout>
