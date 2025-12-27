bf22758a6cb71f6c2d2f3e41e424000788bd613e
DevAniela <dismal.damsel@gmail.com>
2025-12-27 14:02:06 +0200

    feat: set movies index as homepage
    Details: Redirected the root '/' route to MovieController index method to improve user experience.

--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MovieController;
 use App\Http\Controllers\ReviewController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [MovieController::class, 'index']);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
